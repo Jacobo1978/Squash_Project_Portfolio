92bc8dc9c9b5b024b9d7714eab67097096c42ef8
JacoboRuiz <jacruivel@gmail.com>
2023-09-13 21:48:38 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Navbar from './sections/navbar/Navbar';
 import Header from './sections/header/Header';
 import About from './sections/about/About';
-import Services from './sections/services/Services';
+import Skills from './sections/skills/Skills';
 import Portfolio from './sections/portfolio/Portfolio';
 import Testimonials from './sections/testimonials/Testimonials';
-import FAQs from './sections/faqs/FAQs';
+import Education from './sections/education/Education';
 import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
 import FloatingNav from './sections/floating-nav/FloatingNav';
@@ -15,10 +15,10 @@ const App = () => {
       <Navbar/>
       <Header/>
       <About/>
-      <Services/>
+      <Skills/>
       <Portfolio/>
       <Testimonials/>
-      <FAQs/>
+      <Education/>
       <Contact/>
       <Footer/>
       <FloatingNav/>
--- src/sections/faqs/FAQs.jsx
+++
@@ -1,9 +0,0 @@
-import './faqs.css'
-
-const FAQS = () => {
-  return (
-    <section id="faqs"><h2>FAQs</h2></section>
-  )
-}
-
-export default FAQS
